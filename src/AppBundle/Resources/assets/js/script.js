1dd53dad63bfac35ab4ae2a5cbdd679173d71c68
rolandkal <rolandka@live.com>
2015-01-30 08:32:50 +0000

    Bug fixes. Added JS Object to collect postitions

--- src/AppBundle/Resources/assets/js/script.js
+++ src/AppBundle/Resources/assets/js/script.js
@@ -1,73 +1,90 @@
 !function($){
+	$( document ).ready(function() {
+		
+		var $grid = $('.grid > .grid-inner'),
+			$blocks = $grid.find('.block'),
+			$snaps = $blocks.find('.snap'),
+			$computers = $('.computer'),
+			$hostname = $('#hostname'),
+			conf = {};
 	
-	var $grid = $('.grid > .grid-inner'),
-		$blocks = $grid.find('.block'),
-		$snaps = $blocks.find('.snap'),
-		$computers = $('.computer');
-
-			
-	function moveComputer( $target, $item ) {
-	  $target.droppable('option', 'accept', $item);
-	  if($item.hasClass('parent')){
-		  $new = $item.clone().removeClass('parent');
-		  addComputers($new);
-		  $new.hide().appendTo( $target ).fadeIn();
-		  $target.droppable('option', 'accept', '.computer');
-	  }else{
-		  $item.fadeOut(function() {
-		    $item.appendTo( $target ).fadeIn();
-		  });
-	  }
-	  
-	}
-
+		function adjustBlocks($blocks){
+		 $blocks.height($blocks.width())
+		}
+		
+		function moveComputer( $target, $item, $hostname ) {
+		  $target.droppable('option', 'accept', $item);
+		  if($item.hasClass('parent')){
+			  $new = $item.clone().removeClass('parent').attr('data-hostname', $hostname.val())
+			  $new.find('.title').text($hostname.val());					  
+			  addComputers($new);
+			  $new.hide().appendTo( $target ).fadeIn(function(){
+				  $target.droppable('option', 'accept', '.computer');
+				  conf[$new.attr('data-hostname')] = $target.attr('data-position');
+				  $hostname.val('');
+			  });
+			  
+		  }else{
+			  $item.fadeOut(function() {
+			    $item.appendTo( $target ).fadeIn(function(){
+					conf[$item.attr('data-hostname')] = $target.attr('data-position');
+			    });
+			    
+			  });
+		  }
+		  
+		}
 	
-	function addComputers($computers){
-		if(!$computers) return;
-		$computers.draggable({
-			cancel: ".remove",
-			revert: "invalid",
-			helper: "clone",
-			cursor: "move",
-			snap: ".snap",
-			start: function( event, ui ){
-				$(ui.helper).width($snaps.width()).height($snaps.height())
-			},
+		
+		function addComputers($computers){
+			if(!$computers) return;
+			$computers.draggable({
+				cancel: ".remove",
+				revert: "invalid",
+				helper: "clone",
+				cursor: "move",
+				snap: ".snap",
+				start: function( event, ui ){
+					$(ui.helper).width($snaps.width()).height($snaps.height())
+					if( $(ui.helper).hasClass('parent') && $hostname.val() == '') alert('Please enter the hostname for this computer.');	
+				},
+			})
+		}
+				
+		adjustBlocks($blocks);
+		addComputers($computers);
+				
+		$snaps.droppable({
+		
+			accept: ".computer",
+			hoverClass: "highlight",
+			drop: function( event, ui ) {
+				moveComputer($(this), ui.draggable, $hostname );
+		    },
+		    out: function(event, ui){
+		        $(this).droppable('option', 'accept', '.computer');
+		    }  
+		    
 		})
-	}
+		
+		$('.snap').on('click', 'i.remove', function(e){
+			$(e.delegateTarget).droppable('option', 'accept', '.computer').find('.computer').remove();
 			
+		})
+		
+		$('#getconf').on('click', function(e){
+			e.preventDefault;
+			console.log(conf);
 			
-	addComputers($computers);
-			
-	$snaps.droppable({
+		})
 	
-		accept: ".computer",
-		hoverClass: "highlight",
 		
-		drop: function( event, ui ) {
-	       // moveComputer( ui.draggable );
-	       //$(this).append(ui.draggable.css('position','static'))
-	        
-	        moveComputer($(this), ui.draggable );
-    
-	    },
-	    out: function(event, ui){
-	        $(this).droppable('option', 'accept', '.computer');
-	     }   
-	    
-		
-	})
-	$('.snap').on('click', 'i.remove', function(e){
-		$(e.delegateTarget).droppable('option', 'accept', '.computer').find('.computer').remove();
-		
-	})
-	
-	if($blocks.length > 0){
-		$( document ).ready(function() {
-		  $blocks.height($blocks.width())
-		});	
+
 		$( window ).resize(function() {
-		  $blocks.height($blocks.width())
+			if($blocks.length > 0){
+				adjustBlocks($blocks);
+		  	}
 		});		
-	}
+
+	});	
 }(jQuery)
